4ae2bb29180a54b7c3e266b25922b3817cfd0d94
Souha Achour <78798047+195152@users.noreply.github.com>
2024-04-04 08:23:09 +0100

    Add files via upload
    draw epipolar lines

--- stereovision_souha.py
+++ stereovision_souha.py
@@ -8,29 +8,26 @@ binary_image1 = np.load('CerclesI1.npy')
 print(binary_image1.shape[0])
 binary_image2 = np.load('CerclesI2.npy')
 print(binary_image2.shape)
-center_image1= np.load('CerclesC2.npy')
+center_image1= np.load('CerclesC1.npy')
 print("center_image1",center_image1)
 center_image2= np.load('CerclesC2.npy')
 print("center_image2",center_image2)
 
 
 # Resize the larger image to match the size of the smaller one
-if binary_image2.shape[1] > binary_image1.shape[1] or binary_image2.shape[0] > binary_image1.shape[0]:
-    binary_image2 = cv.resize(binary_image2, (binary_image1.shape[1], binary_image1.shape[0]))
-else:
-    binary_image2 = cv.resize(binary_image2, (binary_image1.shape[1], binary_image1.shape[0]))
-
-
-
+#if binary_image2.shape[1] > binary_image1.shape[1] or binary_image2.shape[0] > binary_image1.shape[0]:
+#    binary_image2 = cv.resize(binary_image2, (binary_image1.shape[1], binary_image1.shape[0]))
+#else:
+#    binary_image2 = cv.resize(binary_image2, (binary_image1.shape[1], binary_image1.shape[0]))
 
 # Object points in 3D space
 objp = np.array([
     [center_image1[0][0], center_image1[0][1], 0],
-    [center_image1[1][0], center_image1[1][1], 0],
-    [center_image1[2][0], center_image1[2][1], 0],
     [center_image1[3][0], center_image1[3][1], 0],
+    [center_image1[1][0], center_image1[1][1], 0],
     [center_image1[4][0], center_image1[4][1], 0],
-    [center_image1[6][0], center_image1[6][1], 0]
+    [center_image1[2][0], center_image1[2][1], 0],
+    [center_image1[5][0], center_image1[5][1], 0]
 ], dtype=np.float32)
 print("objp",objp)
 print("objp[0]",objp[0])
@@ -38,30 +35,30 @@ print("objp[0]",objp[0])
 # Image points in 2D space for image 2
 objp2 = np.array([
     [center_image2[0][0], center_image2[0][1],0],
-    [center_image2[1][0], center_image2[1][1],0],
-    [center_image2[2][0], center_image2[2][1],0],
     [center_image2[3][0], center_image2[3][1],0],
+    [center_image2[1][0], center_image2[1][1],0],
     [center_image2[4][0], center_image2[4][1],0],
+    [center_image2[2][0], center_image2[2][1],0],
     [center_image2[6][0], center_image2[6][1],0]
 ], dtype=np.float32)
 print("objp2",objp2)
 # Image points in 2D space for image 1
 imgpoints1 = np.array([
     [center_image1[0][0], center_image1[0][1]],
-    [center_image1[1][0], center_image1[1][1]],
-    [center_image1[2][0], center_image1[2][1]],
     [center_image1[3][0], center_image1[3][1]],
+    [center_image1[1][0], center_image1[1][1]],
     [center_image1[4][0], center_image1[4][1]],
-    [center_image1[6][0], center_image1[6][1]]
+    [center_image1[2][0], center_image1[2][1]],
+    [center_image1[5][0], center_image1[5][1]]
 ], dtype=np.float32)
 
 # Image points in 2D space for image 2
 imgpoints2 = np.array([
     [center_image2[0][0], center_image2[0][1]],
-    [center_image2[1][0], center_image2[1][1]],
-    [center_image2[2][0], center_image2[2][1]],
     [center_image2[3][0], center_image2[3][1]],
+    [center_image2[1][0], center_image2[1][1]],
     [center_image2[4][0], center_image2[4][1]],
+    [center_image2[2][0], center_image2[2][1]],
     [center_image2[6][0], center_image2[6][1]]
 ], dtype=np.float32)
 
@@ -77,14 +74,17 @@ ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, [imgpoints1, imgpoi
 print(ret)
 print(tvecs)
  
- 
-print("rvecs",rvecs[0])
+print(rvecs)
+print("rvecs1",rvecs[0])
+print("rvecs2",rvecs[1])
 ##matrice de rotation => convertir le vecteur en matrice
+print("(rvecs[0])[0] ",(rvecs[0])[0] )
 rmatRight = cv.Rodrigues(rvecs[0])[0] 
 print("rmatRight",rmatRight)
 rmatLeft = cv.Rodrigues(rvecs[1])[0]  
 
-
+print("tvecs[0]",tvecs[0])
+print("tvecs[1]",tvecs[1])
 #matrice complète [R|t] => ajouter t dans R
 rotMatRight = np.concatenate((rmatRight,tvecs[0]), axis=1)
 rotMatLeft = np.concatenate((rmatLeft,tvecs[1]), axis=1)
@@ -106,94 +106,128 @@ def crossMat(v):
                      [v[2], 0, -v[0]],
                      [-v[1], v[0], 0]])
 
-def matFondamental(camLeft, centerRight, camRight):
-    print("a",np.array((crossMat(camLeft @ centerRight)) @ camLeft @ np.linalg.pinv(camRight)))
-    return np.array((crossMat(camLeft @ centerRight)) @ camLeft @ np.linalg.pinv(camRight))
+def matFondamental(camLeft,centerRight,camRight):
+        
+        return np.array((crossMat(camLeft @ centerRight)) @ camLeft @ np.linalg.pinv(camRight))
     
 
-F = matFondamental(camLeft,camWorldCenterLeft,camRight)
-
-def evaluer_contrainte_epipolaire(F, x_left, x_right):
-    # Transposer x_left pour obtenir x_left'
-    x_left_transpose = np.transpose(x_left)
-    # Calculer x'^T * F * x
-    contrainte_epipolaire = np.dot(np.dot(x_left_transpose, F), x_right)
-    return contrainte_epipolaire
-verif = evaluer_contrainte_epipolaire(F, objp2[0], objp[0])
-print("verif",verif)
 
 
 
-print(F)
-# Liste des valeurs de x'^T F x pour chaque paire de points correspondants
-values = [
-    np.dot(np.dot(objp[i], F), objp2[i]) for i in range(len(objp))
-]
 
-# Imprimer les valeurs
-for i, value in enumerate(values):
-    print("Valeur pour la paire de points", i, ":", value)
+def mark_circle_centers(binary_image, center_image):
+    # Create a new image with the same dimensions as binary_image
+    center_marked_image = np.zeros_like(binary_image)
+    
+    # Iterate through each center and mark it as 1
+    for center in center_image:
+        # Retrieve center coordinates
+        center_x, center_y = int(center[1]), int(center[0])  # Convert to integers
+        
+        # Ensure the center coordinates are within the bounds of the image
+        if 0 <= center_x < binary_image.shape[1] and 0 <= center_y < binary_image.shape[0]:
+            # Mark the center as 1 in the new image
+            center_marked_image[center_y, center_x] = 1
+    
+    return center_marked_image
 
 
-def getEpipolarLines(F, points):
+
+# Mark circle centers in both binary images
+center_marked_image1 = mark_circle_centers(binary_image1, center_image1)
+center_marked_image2 = mark_circle_centers(binary_image2, center_image2)
+
+# Display the shape of the new images
+print("Shape of center_marked_image1:", center_marked_image1)
+print("Shape of center_marked_image2:", center_marked_image2.shape)
+
+plt.imshow(center_marked_image1)
+plt.show()
+
+
+def getEpiLines(F, points):
     return F @ points
-print(F)
-print(objp)
-listI2 = []
-for x1 in objp:
-    I2=getEpipolarLines(F,x1)
-    listI2.append(I2)
-print(listI2)
-""" I2=getEpipolarLines(F,imgpoints1())
-print(I2) """
-""" for l in range(1,3):
-        strp = os.path.join(path,'binary_image'+str(l) + '.png')
-        print(strp)
-        image = plt.imread(strp)
-        plt.imshow(image)
-        plt.show() """
-print("obj2",objp2)
-def findEpipiline(path, objp2, F):
-    epipilines = []
-    for center in objp2:
-        epipilinesRight = getEpipolarLines(F, center)
-        print("epipilinesRight",epipilinesRight)
-        tempEpipiline = [center, epipilinesRight]  # Create a new list containing center and epipilinesRight
-        epipilines.append(tempEpipiline)
-    return epipilines
 
-epl = findEpipiline('CerclesC2.png',objp2,F)
-for entry in epl:
-    # Extraire les coordonnées des deux points de la ligne épipolaire
-    point1 = entry[0]  # Premier point
-    print("point1",point1)
-    point2 = entry[1]  # Deuxième point
-    print("point2",point2)
-epl=np.array(epl)
-print("epl[1][0]",epl[1][0])
-# Assurez-vous que binary_image2 est une image en niveaux de gris avec une profondeur de couleur prise en charge
-binary_image2 = binary_image2.astype(np.uint8)
 
-# Tracé des épilignes
-def drawEpilines(img, lines, pts):
-    img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-    for line, pt in zip(lines, pts):
-        color = tuple(np.random.randint(0, 255, 3).tolist())
-        # Extraire les coordonnées x et y des points
-        x0, y0 = int(pt[0]), int(pt[1])
-        # Calculer le deuxième point en utilisant la pente de l'épiligne
-        x1 = int(x0 + 1000 * (-line[1])) # 1000 est une valeur arbitraire pour la longueur de la ligne
-        y1 = int(y0 + 1000 * line[0])
-        # Tracer la ligne
-        img = cv.line(img, (x0, y0), (x1, y1), color, 1)
-        # Tracer un petit cercle à l'extrémité du premier point pour indication
-        img = cv.circle(img, (x0, y0), 5, color, -1)
-    return img
+def findEpilines(path,center_image):
+    epilines = [] # liste de chaque point gauche associe à sa ligne epipolaire
+    for l in range(1):  #parcourt dossier images (26)
+        
+            strp = path 
+    
+    strp = path
+    
+        
+        
+    red = mark_circle_centers(strp, center_image)
+    print("red",red)
+    tempEpilines = []  #epiline associées a leurs points gauches temporairement 
+    pointsLeft = [[],[],[]]
+#Trouver Les points left'''        
+    #i = lindice de chaque ligne epipolaire
+    for i, line in enumerate(red):  #line = une liste de pixels rouges
+        for pixel in line:
+            if pixel != 0:
+                pixel = 1
+        try:
+            #weighted average => (0*0 + 1*0 + 2*0 + ... + 1248 * 1 + 1249 * 0) / n° of red pixels
+            #for instance => (1261+1262+1267)/3 = 1263.33
+            #give position of the red line in x axis
+            pointsLeft[0].append(np.average(range(264), weights = line)) #calcul des points de chaque ligne epipolair
+                                    # moyenne pondere : on parcourt limage horizantalement de position 0 a position 1920
+                                    #ces positions sont multipliés par la valeur du pixel(0 ou 1 cad rouge ou non)
+            pointsLeft[1].append(i) # y axis
+            pointsLeft[2].append(1)  
+        except:
+            pass
+        #A partir de la ligne rouge de l'image de gauche, trouver l'épiline correspondante sur l'image de droite.
+        #Trouver les épilines sur l'image de droite de tous les points rouges de l'image de gauche.
 
-# Tracé des épilignes sur l'image de gauche
-img_with_epilines = drawEpilines(binary_image2, epl[:, 1], objp2)
+            #Calculer les epilines grace a la fonction getEpilines''' 
+        
+    print("pointsLeft",pointsLeft)    
+    epilinesRight = getEpiLines(Fondamental, pointsLeft)
+    
+    tempEpilines.append(pointsLeft)
+    tempEpilines.append(epilinesRight)
+    epilines.append(tempEpilines)  # # liste de chaque point gauche associe à sa aligne epipolaire
+    print("epilines",epilines)
+    return epilines  
 
-# Affichage de l'image avec les épilignes tracées
-cv.imshow('Image avec épilignes', img_with_epilines)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+Fondamental = matFondamental(camRight,camWorldCenterLeft,camLeft)
+
+
+epl = findEpilines(binary_image2, center_image2)  #liste de chaque point gauche associe à sa lignes epipolaires de la camera droite stocke sous forme de matrice a 2 colonnes
+#scan gauche vers droite
+epl= np.array(epl)
+coef , length = epl[0][1].shape
+print("coef , length ",coef , length )
+print(epl.shape)
+print(epl[0][1])
+
+def lineY(coefs,x):
+    a,b,c = coefs
+    return-(c+a*x)/b
+print()
+
+def drawEpl(fname,EplRight):
+    print(EplRight)
+    #img = cv.imread(fname)
+    img=fname
+    
+    coef , length = EplRight.shape #shape=number of elements in each dimension.
+                                    #coef= nbre de lignes , length = nbre colonnes
+    print(coef)
+    print(length)
+    print(coef)
+    print(length)
+    
+    for i in range(0,length): #40 = le pas, pn dessine les epilines avec un intervalle de 40 verticalement donc jsq 1080
+        plt.plot([0,264],[lineY(EplRight[:,i],0),lineY(EplRight[:,i],264)],'y')
+        
+        
+    plt.imshow(img)
+    plt.show()
+
+drawEpl(binary_image1,epl[0][1])
