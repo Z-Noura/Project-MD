6c963a07e985d0f431ede55be4b8dbdfd765247d
Noura Zaruali <noura.zaruali@gmail.com>
2024-05-06 09:01:44 +0200

    Commit of sync

--- convert.m
+++ convert.m
@@ -1,11 +1,11 @@
 clear, close all;
 
 %% load the sample data
-mesh = stlread('Marqueur 2 v8.stl');
+mesh = stlread('Assembly v1.stl');
 
 
 % first, generate a surface from the original image
 % similar to demo_shortcuts_ex1.m
 
 [img, v2smap]=s2v(mesh.Points,mesh.ConnectivityList,50);
-save('BcpDeCercles.mat','img');
+save('Assemblage.mat','img');
